3377d160ab8395be633f93785e4216aa522bb012
Neizmailova Natalia <natata.neizmailova@gmail.com>
2023-03-21 10:44:18 +0200

    main code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
+import Filter from './Filter/Filter';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import { getContactList } from '../redux/features/contactListSlice';
+import { useSelector } from 'react-redux';
+
+const App = () => {
+  const contactList = useSelector(getContactList);
+
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <ContactForm />
+      {contactList.length === 0 ? (
+        <p>There are no contacts</p>
+      ) : (
+        <>
+          <Filter />
+          <ContactList />
+        </>
+      )}
     </div>
   );
 };
+
+export default App;
